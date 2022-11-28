5bc823470d1772890c64a78f67665970857181ca
Jevs9 <ingenierovivas9@gmail.com>
2022-11-27 21:15:23 -0500

    BootCamp - Intro Python - Ejercicio 1 En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
    Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.

---
+++ test.py
@@ -0,0 +1,7 @@
+from operaciones import *
+a = 6
+b = 3
+print(f' Suma: {a}+{b}= {suma(a, b)}')
+print(f' Resta: {a}-{b}= {resta(a, b)}')
+print(f' Multiplicación: {a}*{b}= {multi(a, b)}')
+print(f' división: {a}/{b}= {div(a, b)}')
